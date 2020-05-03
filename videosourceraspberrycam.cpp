e87b23715a548b1165114405abc2c9a64d385d8b
Falko Kellner <kellner@appkellner.de>
2020-05-03 16:46:43 +0200

    make image res configurable

--- videosourceraspberrycam.cpp
+++ videosourceraspberrycam.cpp
@@ -17,8 +17,8 @@ static const int s_defaultWb_r = 200;
 static const int s_defaultWb_b = 200;
 static const int s_defaultIso = 400;
 static const int s_defaultShutter = 40000;
-static const int s_imageWidth = 640;
-static const int s_imageHeight = 480;
+static const int s_defaultImageWidth = 640;
+static const int s_defaultImageHeight = 480;
 static const int s_maxShutter = 330000;
 static const int s_minIso = 100;
 static const int s_maxIso = 800;
@@ -47,6 +47,8 @@ void VideoSourceRaspberryCam::readSettings()
   m_settings["whitebalance_b"] = settings.value("whitebalance_b", s_defaultWb_b).toInt();
   m_settings["iso"] = settings.value("iso", s_defaultIso).toInt();
   m_settings["shutter"] = settings.value("shutter", s_defaultShutter).toInt();
+  m_settings["width"] = settings.value("width", s_defaultImageWidth).toInt();
+  m_settings["height"] = settings.value("height", s_defaultImageHeight).toInt();
   settings.endGroup();
 }
 
@@ -61,6 +63,8 @@ void VideoSourceRaspberryCam::writeSettings()
   settings.setValue("whitebalance_b", m_settings["whitebalance_b"].toInt());
   settings.setValue("iso", m_settings["iso"].toInt());
   settings.setValue("shutter", m_settings["shutter"].toInt());
+  settings.setValue("width", m_settings["width"].toInt());
+  settings.setValue("height", m_settings["height"].toInt());
   settings.endGroup();
 }
 
@@ -148,7 +152,10 @@ bool VideoSourceRaspberryCam::initializeCamera()
 {
   m_camera = raspicam_create_camera();
 
-  raspicam_set_dims(m_camera, s_imageWidth, s_imageHeight);
+  const int width = m_settings["width"].toInt(s_defaultImageWidth);
+  const int height = m_settings["height"].toInt(s_defaultImageHeight);
+
+  raspicam_set_dims(m_camera, width, height);
 
   setCameraSettings(m_settings);
 
@@ -158,7 +165,7 @@ bool VideoSourceRaspberryCam::initializeCamera()
     emit statusChanged("camera open error", true);
     return false;
   }
-  m_currentImage = QImage( s_imageWidth, s_imageHeight, QImage::Format_RGB888 );
+  m_currentImage = QImage( width, height, QImage::Format_RGB888 );
 
   qDebug() << "camera is okay";
   return true;
