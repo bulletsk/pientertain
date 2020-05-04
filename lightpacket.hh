eae42396ef3809bb53759f5e1a005a5607851ded
Falko Kellner <kellner@appkellner.de>
2020-05-05 01:22:43 +0200

    remove unused structs

--- lightpacket.hh
+++ lightpacket.hh
@@ -8,23 +8,6 @@ class LightPacket : public QByteArray
 {
 public:
 
-  struct PacketHeader {
-    uint8_t protocol[9];
-    uint8_t versionMajor;
-    uint8_t versionMinor;
-    uint8_t sequenceNumber;
-    uint16_t reserved1;
-    uint8_t colorSpace;
-    uint8_t reserved2;
-  };
-
-  struct LightData {
-    uint16_t id;
-    uint16_t r;
-    uint16_t g;
-    uint16_t b;
-  };
-
   LightPacket(bool useRGB, bool useGammaCompensation);
 
   void setSequenceNumber(int num);
